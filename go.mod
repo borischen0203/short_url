39d10f593e2049e75c22856728d1a226e1f3a9a4
YI-MING Chen <borischen0203@gmail.com>
2020-12-19 23:01:39 +0800

    update mod and redis file

---
+++ go.mod
@@ -0,0 +1,11 @@
+module short_url
+
+go 1.15
+
+require (
+	github.com/go-redis/redis/v8 v8.4.4
+	github.com/gorilla/mux v1.8.0
+	github.com/speps/go-hashids v2.0.0+incompatible
+	github.com/stretchr/testify v1.6.1
+	go.mongodb.org/mongo-driver v1.4.4
+)
--- main.go
+++ main.go
@@ -13,26 +13,51 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	controller "short_url/controller"
 	mongoDB "short_url/module"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
 
-var client *redis.Client
+// var client *redis.Client
+
+var ctx = context.Background()
+
+func ExampleClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "Hello", "world", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "Hello").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Hello", val)
+
+	val2, err := rdb.Get(ctx, "go").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("go", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
 
 func main() {
 	fmt.Println("Server start")
-	client = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-	comments, err := client.LRange(context.Background(), "comments", 0, 10).Result()
-	if err != nil {
-		return
-	}
-	fmt.Println(comments)
-
+	ExampleClient()
 	router := mux.NewRouter()
 
 	//Init Database
